5d3d5eb12f25f6046fd8e7a62455f9ddbc11ee74
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-05-13 18:39:17 +0200

    add the route for login

---
+++ src/pages/Login/Login.vue
@@ -0,0 +1,20 @@
+
+<template>
+ <p>login</p>
+   
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+        data: []     
+    }
+        },
+    mounted() {
+       
+    }
+    }
+
+</script>
--- src/router.js
+++ src/router.js
@@ -1,18 +1,19 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
-
+import { createMemoryHistory, createRouter } from 'vue-router';
 import LandingPage from './pages/Landing page/LandingPage.vue';
 import Post from './pages/Post/Post.vue';
 import ApplyingForm from './pages/ApplyingForm/ApplyingForm.vue';
+import Login from './pages/Login/Login.vue';
 
 const routes = [
   { path: '/', component: LandingPage },
   { path: '/posts/:id', component: Post },
-  { path: '/application/:id' , component: ApplyingForm}
-]
+  { path: '/application/:id', component: ApplyingForm },
+  { path: '/login', component: Login } 
+];
 
 const router = createRouter({
   history: createMemoryHistory(),
   routes,
-})
+});
 
 export default router;
