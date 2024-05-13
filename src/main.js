a15527aa9b6772b6a68dd63e52935e5b12aa564a
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-05-13 09:11:10 +0300

    install PrimeVue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(router).use(PrimeVue).mount('#app')
