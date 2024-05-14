c54ca419cd3b0a8979659a2a51f9f48bfc3ce96a
SeifOSallam <seif.osallam@gmail.com>
2024-05-14 13:55:56 +0300

    stashing

---
+++ src/pages/CandidateProfile/CandidateProfile.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from '../../components/Navbar.vue';
+import axios from 'axios';
+</script>
+
+<template>
+    <Navbar/>
+
+
+</template>
+
+
+<script>
+
+</script>
--- src/router.js
+++ src/router.js
@@ -3,11 +3,13 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import LandingPage from './pages/Landing page/LandingPage.vue';
 import Post from './pages/Post/Post.vue';
 import ApplyingForm from './pages/ApplyingForm/ApplyingForm.vue';
+import CandidateProfile from './pages/CandidateProfile/CandidateProfile.vue';
 
 const routes = [
   { path: '/', component: LandingPage },
   { path: '/posts/:id', component: Post },
-  { path: '/application/:id' , component: ApplyingForm}
+  { path: '/application/:id' , component: ApplyingForm},
+  { path: '/candidate/profile', component: CandidateProfile},
 ]
 
 const router = createRouter({
