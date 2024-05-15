330e7d6b64338933760b1cc8a636e9f64b3584d0
SeifOSallam <seif.osallam@gmail.com>
2024-05-15 04:09:05 +0300

    fixes

--- src/components/CandidateComponents/AccountDetails.vue
+++ src/components/CandidateComponents/AccountDetails.vue
@@ -5,42 +5,42 @@
             <form>
                 <div class="mb-3">
                     <label class="small mb-1" for="inputFirstName">Name</label>
-                    <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your name" v-model="name" @blur="v.name.$touch">
-                    <div v-if="v.name.$error">Name field has an error.</div>
+                    <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your name" v-model="name">
+                    <small class="text-danger" v-if="v.name.$error">{{ v.name.$errors[0].$message }}</small>
                 </div>
 
                 <!-- Form Group (username)-->
                 <div class="mb-3">
                     <label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
-                    <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" v-model="user.username">
+                    <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" v-model="username">
                 </div>
                 
                 <div class="mb-3">
                     <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                    <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" v-model="user.email">
+                    <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" v-model="email">
                 </div>
                 <!-- Form Row        -->
                 <div class="row gx-3 mb-3">
                     <!-- Form Group (faculty name)-->
                     <div class="col-md-6">
                         <label class="small mb-1" for="inputFacName">Faculty name</label>
-                        <input class="form-control" id="inputFacName" type="text" placeholder="Enter your faculty name" v-model="user.faculty">
+                        <input class="form-control" id="inputFacName" type="text" placeholder="Enter your faculty name" v-model="faculty">
                     </div>
                     <!-- Form Group (education)-->
                     <div class="col-md-6">
                         <label class="small mb-1" for="inputEducation">Education</label>
-                        <input class="form-control" id="inputEducation" type="text" placeholder="Enter your Education" v-model="user.education">
+                        <input class="form-control" id="inputEducation" type="text" placeholder="Enter your Education" v-model="education">
                     </div>
                 </div>
                 <!-- Form Group (city)-->
                 <div class="mb-3">
                     <label class="small mb-1" for="inputCity">City</label>
-                    <input class="form-control" id="inputCity" type="text" placeholder="Enter your city" v-model="user.city">
+                    <input class="form-control" id="inputCity" type="text" placeholder="Enter your city" v-model="city">
                 </div>
                 <!-- Form Group (email address)-->
                 
                 <!-- Save changes button-->
-                <button class="btn btn-primary" type="button" @click="submit(v)">Save changes</button>
+                <button class="btn btn-primary" type="button" @click="submit()">Save changes</button>
             </form>
         </div>
     </div>
@@ -48,7 +48,7 @@
 
 <script>
     import useVuelidate from '@vuelidate/core'; 
-    import { minLength, required } from '@vuelidate/validators';
+    import { email, minLength, required } from '@vuelidate/validators';
     export default {
         props: ['user'],
         setup() {
@@ -69,16 +69,18 @@
         validations() {
             return {
                 name : { required, minLength: minLength(10) },
-                email : { required },
+                email : { required, email },
                 username : { required },
             }
         },
+        validationConfig: {
+            $autoDirty: true,
+            $lazy: true,
+        },
         methods: {
-            async submit(v) {
-                console.log(v);
-                console.log("name:" + JSON.stringify(v.name));
-                console.log("name errors:" + JSON.stringify(v.name.$errors));
-                const result = await v.$validate();
+            async submit() {
+                console.log(this.v.$errors);
+                const result = await this.v.$validate();
 
                 if (!result) {
                     console.log("ERROR");
