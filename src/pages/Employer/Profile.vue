4c3e7bf44a5340f257fb9067f7a7680299ea49b0
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-05-16 02:58:10 +0300

    Employer Profile Design enhancement

--- src/pages/Employer/Profile.vue
+++ src/pages/Employer/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="wrapper">
     <Navbar />
     <div class="p-3 row">
       <div v-if="employer" class="col-3" >
@@ -68,51 +68,69 @@
 
           </template>
         </Card>
-<!-- 
-        <div>
-            <p class="mx-3 fw-bold my-3">City</p>
-            <select class="form-select w-50 mx-3" name="city" @change="getJobs">
-              <option value="all">All</option>
-              <option value="approved">Approved</option>
-              <option value="pending">Pending</option>
-              <option value="rejected">Rejected</option>
-            </select>
-        </div> -->
-      </div>
 
-      <div v-else>
-        <p>Loading...</p>
-      </div>
+      </div> <!-- if employer -->
+
+      <div v-else class="col-3">
+          <Card class="mb-2" >
+             <template #content>
+                <Skeleton shape="circle" size="4rem" class="mr-2 mb-2"></Skeleton>
+                <Skeleton height="1rem" class="mb-2" borderRadius="16px"></Skeleton>
+                <Skeleton height="1rem" class="mb-2" borderRadius="16px"></Skeleton>
+                <Skeleton height="1rem" class="mb-2" borderRadius="16px"></Skeleton>
+             </template>
+           </Card>
+      </div> <!-- else employer -->
 
       <div id="jobs" class="col-8" >
           <div v-if="jobs" > 
-            <PostCard  v-for="job in jobs" :key="job.id" class="mb-2"
-              :job_title="job.job_title"
-              :location="job.location"
-              :application_deadline="job.application_deadline"
-              :created_at="job.created_at"
-              :logo="employer.company_logo "
-              :work_type="job.work_type"
-              :start_salary="job.start_salary"
-              :end_salary="job.end_salary"
-              :post_id="job.id"
-              :company="employer.company_name"
-              :route="`/posts/${job.id}`"
-              />
-            
-     
-          </div>
+              <div v-if="jobs.length !=0">
+                <PostCard  v-for="job in jobs" :key="job.id" class="mb-2"
+                  :job_title="job.job_title"
+                  :location="job.location"
+                  :application_deadline="job.application_deadline"
+                  :created_at="job.created_at"
+                  :logo="employer.company_logo "
+                  :work_type="job.work_type"
+                  :start_salary="job.start_salary"
+                  :end_salary="job.end_salary"
+                  :post_id="job.id"
+                  :company="employer.company_name"
+                  :route="`/posts/${job.id}`
+                  "/>
+                <MyPaginator :paginationData="{ links: paginationLinks, next: next, prev: prev }" @page-change="handlePageChange" /> 
+              </div>
+              <div v-else>
+                  <Message severity="contrast">You don't have any jobs for now.</Message>
+              </div>
+          </div><!-- if jobs -->
 
           <div v-else>
-            <p>Loading...</p>
-          </div>
-          <MyPaginator :paginationData="{ links: paginationLinks, next: next, prev: prev }" @page-change="handlePageChange" />
-                 
-       </div>
+            <div class="card">
+              <div class="border-round border-1 surface-border p-4 surface-card">
+                  <div class="flex mb-3">
+                      <Skeleton shape="circle" size="4rem" class="mr-2"></Skeleton>
+                      <div>
+                          <Skeleton width="10rem" class="mb-2"></Skeleton>
+                          <Skeleton width="5rem" class="mb-2"></Skeleton>
+                          <Skeleton height=".5rem"></Skeleton>
+                      </div>
+                  </div>
+                  <Skeleton width="100%" height="150px"></Skeleton>
+                  <div class="flex justify-content-between mt-3">
+                      <Skeleton width="4rem" height="2rem"></Skeleton>
+                      <Skeleton width="4rem" height="2rem"></Skeleton>
+                  </div>
+              </div>
 
-      </div>
+            </div>
+          </div> <!-- else  jobs -->
+                 
+      </div> <!-- jobs -->
+
+    </div> <!-- row -->
   
-  </div>
+  </div> <!-- wrapper -->
 </template>
 
 
@@ -127,6 +145,8 @@ import InputGroup from 'primevue/inputgroup';
 import InputGroupAddon from 'primevue/inputgroupaddon';
 import InputText from 'primevue/inputtext';
 import Avatar from 'primevue/avatar';
+import Message from 'primevue/message';
+import Skeleton from 'primevue/skeleton';
 import Paginator from 'primevue/paginator';
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
@@ -139,7 +159,7 @@ var static_employer_id = 1;
 
 export default {
    components:{ Button, Navbar ,InputGroup,InputText ,InputGroupAddon,Card,Avatar,Paginator , 
-   TabView , TabPanel ,MyPaginator , PostCard},
+   TabView , TabPanel ,MyPaginator , PostCard,Message,Skeleton},
       data:()=>(
         {
           v$:useVuelidate(),
