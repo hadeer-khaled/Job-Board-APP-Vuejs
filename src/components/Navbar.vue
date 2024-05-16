b5082be63d5b9a29b5763c9ba9365039ce7a11cd
omarkhalil117 <omarkhalil117@gmail.com>
2024-05-16 10:31:32 +0300

    add navbar conditional rendering for different roles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,8 +8,32 @@
             <img  src="/logo.jpg" class="mx-3 border rounded-circle" style="width:50px"/>
         </div>
     <RouterLink to="/" class="text-decoration-none mx-3 my-auto fw-bold ">Home</RouterLink>
+
+        <!-- Admin -->
+        <div v-if="role === 'admin'" class="d-flex align-items-center justify-content-around">
+            <RouterLink class="mx-3 text-decoration-none fw-bold ">Posts</RouterLink>
+            <RouterLink class="mx-3 text-decoration-none fw-bold ">Users</RouterLink>
+        </div>
+        
+        <!-- Candidate -->
+        <div v-if="role === 'candidate'" class="d-flex align-items-center justify-content-around">
+            <RouterLink to="/candidate/profile" class="mx-3 text-decoration-none fw-bold ">Profile</RouterLink>
+            <RouterLink to="/candidate/applications" class="mx-3 text-decoration-none fw-bold ">My Applications</RouterLink>
+        </div>
+
+        <!-- Employer -->
+        <div v-if="role === 'employer'" class="d-flex align-items-center justify-content-around">
+            <RouterLink to="/employer/profile" class="mx-3 text-decoration-none fw-bold ">Profile</RouterLink>
+        </div>
+        
+
+        <div>
+
+        </div>
     </div>
-    <div>
+
+
+    <div v-if="!loggedUser">
         <div class="d-flex">
 
             <span style="height:40px;width:1px;background-color:#D9DDE4;margin-right:16px"></span>
@@ -18,6 +42,27 @@
         </div>
     </div>
 
+    <div v-if="loggedUser">
+        <div class="d-flex align-items-center  justify-content-center ">
+        <p class="mx-3 my-auto">User Name</p>
+        <img v-if="!image" style="width: 50px;" src="/default_user1.png" alt="user img">
+        <img v-if="image" style="width: 50px;" :src="image" alt="user img">
+        </div>
+    </div>
+
 </nav>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            loggedUser: true,
+            username: '',
+            image: '',
+            role: 'employer',
+        }
+    },
+}
+</script>
