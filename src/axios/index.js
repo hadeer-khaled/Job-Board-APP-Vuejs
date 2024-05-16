145e88dcc83bfff18a180030576fafa083c476fb
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-05-16 20:52:56 +0300

    add baseUrl from .env

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: `${import.meta.env.VITE_BASE_URL}`,
 });
 // to makeuse authorization in backedn server
 axiosInstance.interceptors.request.use(
